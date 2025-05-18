e20b151c81bda0f2b231cb404f388f8f42b82c97
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-05-18 23:23:00 +0300

    Project structure

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -1,5 +1,26 @@
+<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path, include
+=======
+"""
+URL configuration for alx_travel_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+>>>>>>> 840e1fe (Project structure)
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -9,6 +30,7 @@ schema_view = get_schema_view(
       title="ALX Travel App API",
       default_version='v1',
       description="API documentation for ALX Travel App",
+<<<<<<< HEAD
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@alxtravelapp.local"),
    public=True,
@@ -20,3 +42,15 @@ urlpatterns = [
     path('listings/', include('listings.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+=======
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('api/', include('listings.urls')),  # Uncomment if listings/urls.py exists
+]
+>>>>>>> 840e1fe (Project structure)
